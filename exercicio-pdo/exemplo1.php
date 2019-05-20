365a59009684b892d45bdb92abb23f8e36738327
Gabrielle <gabrielleolives1@gmail.com>
2019-05-20 13:57:01 -0300

    subindo exercicios

---
+++ exercicio-pdo/exemplo1.php
@@ -0,0 +1,27 @@
+<?php 
+	//tipo de banco, nome do banco e aonde ele esta
+	$conn = new PDO("mysql:dbname=dbGabi;host=192.168.14.224", "usr_teste", "1234");
+
+	$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	
+	$stmt->execute();
+
+	//fatia todas as linhas 
+	$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC); //TRAZ SÓ O ASSOCIATIVO
+
+	foreach ($resultado as $row) {
+		foreach ($row as $key => $value) {
+
+			echo "<strong>" . $key."</strong>". $value."<br>"; 
+
+		}
+
+		echo "========================================<br>";
+	}
+
+
+
+
+
+
+?> 
